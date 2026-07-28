9321faf94db67cf0ce3fd9f88ed2c0a193073a83
Kushal Shrestha <kushalstha958@gmail.com>
2026-07-28 10:49:22 +0545

    Edited url

--- frontend/src/api/habitapi.js
+++ frontend/src/api/habitapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
+  baseURL: "https://habit-tracker958.netlify.app/" || "http://localhost:3001",
   withCredentials: true,
 });
 
