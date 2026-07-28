3e69f7273cdf14b8c4ab01c7b6f45e4509a29bea
Kushal Shrestha <kushalstha958@gmail.com>
2026-07-28 10:25:26 +0545

    Added render

--- frontend/src/api/authapi.js
+++ frontend/src/api/authapi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://habittracker-8kse.onrender.com/habits",
   withCredentials: true,
 });
 
